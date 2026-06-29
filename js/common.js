6934185051add10085752b63ee9b8d2d0474a132
seyoung5309 <leenight5309@naver.com>
2026-06-29 11:58:21 +0900

    feat: 0629 수업 끝

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 /**
-* 부가세를 계산해서 리턴하는 함수 
-* @param {*} productPrice
-* @returns
-*/
+ * 부가세를 계산해서 리턴하는 함수
+ * @param {*} productPrice
+ * @returns
+ */
 
 function taxAmount(productPrice) {
   let tax = 0.1;
@@ -16,3 +16,20 @@ function taxAmount(productPrice) {
 function getTop5() {
   return ["a", "b", "c", "d"];
 }
+
+/**
+ * 며칠 전 후를 구하는 함수
+ * @param {*} date
+ * @returns
+ */
+let getIntervalDate = (date) => {
+  let now = new Date();
+  let toDay = now.getTime();
+  let currentDay = 24 * 60 * 60 * 1000 * date;
+  let result = new Date(toDay + currentDay);
+  return (
+    String(result.getMonth() + 1).padStart(2, 0) +
+    "/" +
+    String(result.getDate()).padStart(2, 0)
+  );
+};
